b5a848f73aeb24f55d8ce2f1569d4a62f3378133
capitalado <156638359+capitalado@users.noreply.github.com>
2024-02-12 10:33:59 +0530

    Add files via upload

---
+++ palindromenumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, original, rem, sum = 0;
+            Console.Write("Enter a number to check: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            original = num;
+            while(num != 0){
+                rem = num % 10;
+                sum = (sum*10) + rem;
+                num = num / 10;
+            }
+
+            if (original == sum)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
