34cb655a345cf5477ca178ef603dc551f3e92596
capitalado <156638359+capitalado@users.noreply.github.com>
2024-02-12 10:27:52 +0530

    Add files via upload

---
+++ armstrong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rem, sum = 0, temp;
+            Console.WriteLine("Enter the number: ");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                rem = n % 10;
+                sum = sum + (rem*rem*rem);
+                n = n / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Armstrong number");
+            }
+            else
+            {
+                Console.WriteLine("Not an Armstrong number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
