a69738ca778b33564371df3f69e1cf65df82359c
capitalado <156638359+capitalado@users.noreply.github.com>
2024-02-12 10:25:32 +0530

    Add files via upload

---
+++ constructors.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication20  
+{
+    //Class Rectangle
+    public class Rectangle
+    {
+        public int width, length;
+        public Rectangle()
+        {
+            Console.WriteLine("This is a default constructor!");
+        }
+
+        public Rectangle(int x, int y)
+        {
+            Console.WriteLine("This is a Parameterized constructor!");
+            width = x;
+            length = y;     
+        }
+        public void area() 
+        {
+            int area = width * length;
+            Console.WriteLine("The Area is: " + area);
+        }
+
+    }
+
+    //Main Program starts here
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Rectangle r2 = new Rectangle(3, 4);
+            r2.area();
+
+           Console.ReadKey();
+        }
+    }
+}
