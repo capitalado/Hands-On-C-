7a78eeb2e2c12bb4606910043f797cf9158c85ab
capitalado <156638359+capitalado@users.noreply.github.com>
2024-02-12 10:28:53 +0530

    Add files via upload

---
+++ palindromestring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check: ");
+            String name = Console.ReadLine();
+            String reverse = String.Empty;
+
+            for (int i = name.Length - 1; i >= 0; i--) { 
+            reverse+=name[i];
+            }
+            if (name == reverse)
+            {
+                Console.WriteLine("Is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
