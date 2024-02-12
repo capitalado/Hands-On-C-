b15c53d4948068603132302780fcd2144d5e8205
capitalado <156638359+capitalado@users.noreply.github.com>
2024-02-12 10:26:42 +0530

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fixer;
+            int[] myArr = new int[5];
+            Console.WriteLine("Enter 5 elements: ");
+            for (i = 0; i < 5; i++)
+            {
+                myArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            fixer = myArr.Min();
+            Console.WriteLine("The Minimum number is: " + fixer);
+            Console.ReadKey();
+        }
+    }
+}
